74181f484081bdcbaad014f93c6e22f385061d14
bykowskin <norbertbykowski@gmail.com>
2020-10-22 23:17:27 +0200

    Empty SpringBoot2 project.

---
+++ src/test/java/com/SpringBoot2Course/SpringBoot2CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot2Course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
